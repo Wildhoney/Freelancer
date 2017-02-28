8a5fdab4e77ddc07844e6241b8b89984e94c272e
Wildhoney <adam.timberlake@gmail.com>
2017-02-28 15:24:15 +0000

    Added initial implementation

---
+++ src/freelancer.js
@@ -0,0 +1,49 @@
+/**
+ * @method createBlob
+ * @param {Function} fn
+ * @return {String}
+ */
+const createBlob = fn => {
+
+    if (typeof fn !== 'function') {
+        throw 'Freelancer: Passed parameter must be a function.';
+    }
+
+    const blob = new Blob([`(${fn.toString()})()`], { type: 'application/javascript' });
+    return URL.createObjectURL(blob);
+
+};
+
+/**
+ * @class Freelancer
+ * @extends Worker
+ */
+class Freelancer extends Worker {
+
+    /**
+     * @constructor
+     * @param {Function} fn
+     * @return {Worker}
+     */
+    constructor(fn) {
+        return super(createBlob(fn));
+    }
+
+}
+
+/**
+ * @class SharedFreelancer
+ * @extends SharedWorker
+ */
+export class SharedFreelancer extends SharedWorker {
+
+    /**
+     * @constructor
+     * @param {Function} fn
+     * @return {Worker}
+     */
+    constructor(fn) {
+        return super(createBlob(fn));
+    }
+
+}
