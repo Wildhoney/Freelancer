4911327dbad95530ea04680ea7274af6c901cc83
Wildhoney <adam.timberlake@gmail.com>
2017-03-03 09:29:43 +0000

    Fixed linting

--- example/server/default.js
+++ example/server/default.js
@@ -1,10 +1,8 @@
-import { basename } from 'path';
-import { existsSync } from 'fs';
 import http from 'http';
+import { Z_BEST_COMPRESSION } from 'zlib';
 import express from 'express';
 import compression from 'compression';
 import opener from 'opener';
-import { Z_BEST_COMPRESSION } from 'zlib';
 
 const app = express();
 const server = http.createServer(app);
